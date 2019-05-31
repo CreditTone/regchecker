328056982696f5b82d984119199269bb351bcca3
方卓 <fangzhuo@quicklyun.com>
2019-05-31 13:51:34 +0800

    增加redis相关

---
+++ src/main/kotlin/com/jisucloud/clawler/regagent/controller/AsyncCheckController.kt
@@ -0,0 +1,65 @@
+package com.jisucloud.clawler.regagent.controller
+
+import com.alibaba.fastjson.JSON
+import com.jisucloud.clawler.regagent.entity.Info
+import com.jisucloud.clawler.regagent.service.CheckService
+import com.jisucloud.clawler.regagent.service.TorIDCardSearchService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.redis.core.StringRedisTemplate
+import org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE
+import org.springframework.web.bind.annotation.*
+
+/**
+ * 用于批量查询是否注册
+ */
+@RestController
+class AsyncCheckController {
+
+    @Autowired
+    lateinit var checkService: CheckService
+
+    @PostMapping("async/check", produces = [APPLICATION_JSON_UTF8_VALUE], consumes = [APPLICATION_JSON_UTF8_VALUE])
+    @ResponseBody
+    fun doCheck(@RequestBody requestBody: String): Map<String, Any>? {
+        try {
+            val jsonInfo = JSON.parseObject(requestBody, Info::class.java)
+            val result = checkService.doCheckAsync2(jsonInfo)
+            return mapOf(
+                    "code" to "200",
+                    "success" to true,
+                    "msg" to "成功",
+                    "result" to result
+            )
+        } catch (e: Exception) {
+            e.printStackTrace()
+            return mapOf(
+                    "code" to "400",
+                    "success" to false,
+                    "msg" to e.localizedMessage
+            )
+        }
+        return null
+    }
+
+    @GetMapping("async/get", produces = [APPLICATION_JSON_UTF8_VALUE])
+    @ResponseBody
+    fun doSearchTor(@RequestParam account: String): Map<String, Any>? {
+        val getResult = checkService.doAsyncGetResult(account)
+        if (getResult.first) {
+            return mapOf(
+                    "code" to "200",
+                    "success" to true,
+                    "msg" to "成功",
+                    "result" to getResult.second + ""
+            )
+        } else {
+            return mapOf(
+                    "code" to "400",
+                    "success" to false,
+                    "msg" to "未完成",
+                    "result" to ""
+            )
+        }
+
+    }
+}
--- src/main/kotlin/com/jisucloud/clawler/regagent/service/CheckService.kt
+++ src/main/kotlin/com/jisucloud/clawler/regagent/service/CheckService.kt
@@ -7,12 +7,10 @@ import com.jisucloud.clawler.regagent.entity.Result
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.async
 import kotlinx.coroutines.runBlocking
-import org.jsoup.Connection
-import org.jsoup.Jsoup
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.redis.core.StringRedisTemplate
-import org.springframework.http.MediaType
 import org.springframework.stereotype.Service
+import java.util.concurrent.TimeUnit
 
 @Service
 class CheckService {
@@ -24,21 +22,21 @@ class CheckService {
      * 同步检测
      */
 //    fun doCheck(info: Info): MutableList<Any> {
-//        var relustList = mutableListOf<Any>()
+//        var resultList = mutableListOf<Any>()
 //        SpiderMap.map.forEach { k, v ->
 //            if (!info.exclusions.contains(k)) {
 //                val checkResult = v.checkTelephone(info.account)
-//                relustList.add(Result(v.message(), v.home(), v.platform(), v.platformName(), v.checkEmail(info.account), checkResult, v.fields, v.tags()))
+//                resultList.add(Result(v.message(), v.home(), v.platform(), v.platformName(), v.checkEmail(info.account), checkResult, v.fields, v.tags()))
 //            }
 //        }
-//        return relustList
+//        return resultList
 //    }
 
     /**
      * 多协程加速检测接口响应
      */
 //    fun doCheckAsync(info: Info): MutableList<Any> {
-//        var relustList = mutableListOf<Any>()
+//        var resultList = mutableListOf<Any>()
 //        var list = SpiderMap.map.map {
 //            return@map GlobalScope.async {
 //                val k = it.key
@@ -50,13 +48,13 @@ class CheckService {
 //                    } catch (e: Exception) {
 //                        e.printStackTrace()
 //                    }
-//                    relustList.add(Result(v.message(), v.home(), v.platform(), v.platformName(), v.checkEmail(info.account), checkResult, v.fields, v.tags()))
+//                    resultList.add(Result(v.message(), v.home(), v.platform(), v.platformName(), v.checkEmail(info.account), checkResult, v.fields, v.tags()))
 //                }
 //            }
 //        }
 //        val job007 = GlobalScope.async {
 //            //单独处理Reg007
-//            relustList.addAll(Reg007Service().doCheckTelephone(info.account))
+//            resultList.addAll(Reg007Service().doCheckTelephone(info.account))
 //        }
 //        runBlocking {
 //            list.map {
@@ -64,7 +62,7 @@ class CheckService {
 //            }
 //            job007.await()
 //        }
-//        return relustList
+//        return resultList
 //    }
 
     fun doCheckAsync2(info: Info): MutableList<Any> {
@@ -78,7 +76,7 @@ class CheckService {
 //                    .execute()
 //
 //        }
-        var relustList = mutableListOf<Any>()
+        var resultList = mutableListOf<Any>()
         var list = SpiderMap.map.map {
             return@map GlobalScope.async {
                 val k = it.key
@@ -92,7 +90,7 @@ class CheckService {
                     }
                     //单纯是手机好的时候不调用Email检测方法
                     val checkEmail = if (Regex("\\d{11}").matches(info.account)) false else v.checkEmail(info.account)
-                    relustList.add(Result(v.message(), v.home(), v.platform(), v.platformName(), checkEmail, checkResult, v.fields, v.tags()))
+                    resultList.add(Result(v.message(), v.home(), v.platform(), v.platformName(), checkEmail, checkResult, v.fields, v.tags()))
                 }
             }
         }
@@ -102,6 +100,48 @@ class CheckService {
             }
 //            job007.await()
         }
-        return relustList
+        return resultList
+    }
+
+    /**
+     * 异步查询并存储到redis
+     * key=info.account
+     */
+    fun doAsyncCheckAndSave(info: Info) {
+
+        GlobalScope.async {
+            var resultList = mutableListOf<Any>()
+            var list = SpiderMap.map.map {
+                return@map async {
+                    val k = it.key
+                    val v = it.value.javaClass.newInstance() as PapaSpider
+                    if (!info.exclusions.contains(k)) {
+                        var checkResult = false
+                        try {
+                            checkResult = v.checkTelephone(info.account)
+                        } catch (e: Exception) {
+                            e.printStackTrace()
+                        }
+                        //单纯是手机好的时候不调用Email检测方法
+                        val checkEmail = if (Regex("\\d{11}").matches(info.account)) false else v.checkEmail(info.account)
+                        resultList.add(Result(v.message(), v.home(), v.platform(), v.platformName(), checkEmail, checkResult, v.fields, v.tags()))
+                    }
+                }
+            }
+            runBlocking {
+                list.map {
+                    it.await()
+                }
+                redisTemplate.boundValueOps(info.account).set(JSON.toJSONString(resultList), 30, TimeUnit.MINUTES)
+            }
+        }
+    }
+
+    /**
+     * 获取异步查询结果
+     * key=info.account
+     */
+    fun doAsyncGetResult(account: String): Pair<Boolean, String?> {
+        return if (redisTemplate.hasKey(account)) Pair(true, redisTemplate.boundValueOps(account).get()) else Pair(false, null)
     }
 }
