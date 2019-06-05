6f942afd562f4b72b2cc098c0b5a817ce510e71e
郭钟 <guozhong@quicklyun.com>
2019-06-05 21:24:15 +0800

    从回望撞库

---
+++ src/main/kotlin/com/jisucloud/clawler/regagent/service/impl/work/CongHuiWangSpider.java
@@ -0,0 +1,94 @@
+package com.jisucloud.clawler.regagent.service.impl.work;
+
+import com.jisucloud.clawler.regagent.service.PapaSpider;
+import com.jisucloud.clawler.regagent.util.StringUtil;
+
+import lombok.extern.slf4j.Slf4j;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+@Component
+public class CongHuiWangSpider implements PapaSpider {
+
+	private OkHttpClient okHttpClient = new OkHttpClient.Builder().connectTimeout(10, TimeUnit.SECONDS)
+			.readTimeout(10, TimeUnit.SECONDS).retryOnConnectionFailure(true).build();
+
+	@Override
+	public String message() {
+		return "中国慧聪网,做生意就上慧聪网,360行,行行在慧聪.中国慧聪网 B2B诚信企业,1500万商家,海量供求信息任你选。";
+	}
+
+	@Override
+	public String platform() {
+		return "hc360";
+	}
+
+	@Override
+	public String home() {
+		return "hc360.com";
+	}
+
+	@Override
+	public String platformName() {
+		return "慧聪网";
+	}
+
+	@Override
+	public Map<String, String[]> tags() {
+		return new HashMap<String, String[]>() {
+			{
+				put("媒体", new String[] { });
+			}
+		};
+	}
+
+//	public static void main(String[] args) throws InterruptedException {
+//		System.out.println(new CongHuiWangSpider().checkTelephone("18210538000"));
+//		System.out.println(new CongHuiWangSpider().checkTelephone("18210538513"));
+//	}
+
+	@Override
+	public boolean checkTelephone(String account) {
+		try {
+			String url = "https://reg.hc360.com/reg/check/bindmobile.html";
+			FormBody formBody = new FormBody
+	                .Builder()
+	                .add("mobile", account)
+	                .build();
+			Request request = new Request.Builder().url(url)
+					.addHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:56.0) Gecko/20100101 Firefox/56.0")
+					.addHeader("Host", "reg.hc360.com")
+					.addHeader("Referer", "https://reg.hc360.com/reg/info?sourcetypeid=1517")
+					.post(formBody)
+					.build();
+			Response response = okHttpClient.newCall(request).execute();
+			String res = response.body().string();
+			if (res.contains("1")) {
+				return true;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+
+	@Override
+	public boolean checkEmail(String account) {
+		return false;
+	}
+
+	@Override
+	public Map<String, String> getFields() {
+		return null;
+	}
+
+}
